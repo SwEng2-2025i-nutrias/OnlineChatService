1e37a8be9643c852b3aa780f06dcc624298a854d
Samuel Vargas <sjvc0595@gmail.com>
2025-07-10 18:28:15 -0500

    feat: Add authentication verification to the endpoints

--- adapters/input/chat_controller.py
+++ adapters/input/chat_controller.py
@@ -71,12 +71,20 @@ def create_chat():
 
 # Get a chat by ID endpoint
 @chat_blueprint.route('/chats/<string:chat_id>', methods=['GET'])
+@require_auth
 @swag_from("docs/get_chat.yaml")
 def get_chat(chat_id:str):
     try:
         chat = get_chat_use_case.get_chat(chat_id)
+
+
         if not chat:
             return jsonify({"error": "Chat not found"}), 404
+        
+        user_id = g.user_id
+        if user_id not in [participant["user_id"] for participant in chat.participants]:
+            return jsonify({"error": "You are not a participant of this chat"}), 403
+        
         return jsonify(chat.to_dict()), 200
         
     except ValueError as e:
@@ -84,12 +92,19 @@ def get_chat(chat_id:str):
     
 # Get chats by user ID endpoint
 @chat_blueprint.route('/chats', methods=['GET'])
-@swag_from("docs/get_user_chats.yaml")
+@require_auth
+# @swag_from("docs/get_user_chats.yaml")
 def get_user_chats():
     user_id = request.args.get('user_id')
+
     if not user_id:
         return jsonify({"error": "user_id is required"}), 400
     
+    token_user_id = g.user_id
+
+    if user_id != token_user_id:
+        return jsonify({"error": "You can only get your own chats"}), 403
+
     try:
         chats = get_user_chats_use_case.get_user_chats(user_id)
         return jsonify([chat.to_dict() for chat in chats]), 200
