aadd9cd186f1424c4cad35b017e2a35d2533b5a8
Samuel Vargas <sjvc0595@gmail.com>
2025-07-10 13:15:06 -0500

    feat: implement HttpUserRepository for user data retrieval via HTTP requests

---
+++ adapters/output/http_user_repository.py
@@ -0,0 +1,16 @@
+import requests
+from domain.ports.output.user_repository import UserRepository
+from domain.entities.user import User
+
+from typing import Optional
+
+class HttpUserRepository(UserRepository):
+    def __init__(self, base_url: str):
+        self.base_url = base_url
+
+    def get_by_id(self, user_id: str) -> Optional[User]:
+        response = requests.get(f"{self.base_url}/users/{user_id}")
+        # TODO: Test if the response is correctly formatted
+        if response.status_code == 200:
+            return User.from_dict(response.json())
+        return None
--- domain/entities/user.py
+++ domain/entities/user.py
@@ -1,6 +1,25 @@
+from typing import Dict
+
 class User:
     def __init__(self, user_id: str, email: str, name: str, role: str):
         self.user_id = user_id
         self.email = email
         self.name = name
         self.role = role
+
+    @classmethod
+    def from_dict(cls, data: dict[str, str]):
+        return cls(
+            user_id=data["user_id"],
+            email=data["email"],
+            name=data["name"],
+            role=data["role"]
+        )
+    
+    def to_dict(self) -> Dict[str, str]:
+        return {
+            "user_id": self.user_id,
+            "email": self.email,
+            "name": self.name,
+            "role": self.role
+        }
